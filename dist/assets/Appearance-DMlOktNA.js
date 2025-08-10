62d29dd2ec81582602350bb3f1a70f7585e3b210
Renato <92505604+k1ngr3n1@users.noreply.github.com>
2025-08-10 11:37:53 +0200

    fix

---
+++ dist/assets/Appearance-DMlOktNA.js
@@ -0,0 +1,16 @@
+import{d as m,H as k,c as i,a as t,F as y,m as g,e as s,n as d,p as x,t as b,y as f,u as r,w as l,b as e,g as v}from"./app-Bpr-Z9gF.js";import{c as o}from"./createLucideIcon-DzPYAFwf.js";import{_ as M,a as w}from"./Layout.vue_vue_type_script_setup_true_lang-B7aa-eae.js";import{_ as I}from"./AppLayout.vue_vue_type_script_setup_true_lang-DcOGH0jW.js";import"./AppLogoIcon.vue_vue_type_script_setup_true_lang-DW9_R7OJ.js";import"./useForwardExpose-D7jAvkae.js";import"./RovingFocusGroup-BFaq9z7v.js";import"./settings-D0KQ1_ge.js";import"./x-FtVuPjhG.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const A=o("MonitorIcon",[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=o("MoonIcon",[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const $=o("SunIcon",[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]]),B={class:"inline-flex gap-1 rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800"},S=["onClick"],z={class:"ml-1.5 text-sm"},D=m({__name:"AppearanceTabs",setup(u){const{appearance:a,updateAppearance:c}=k(),p=[{value:"light",Icon:$,label:"Light"},{value:"dark",Icon:C,label:"Dark"},{value:"system",Icon:A,label:"System"}];return(j,q)=>(t(),i("div",B,[(t(),i(y,null,g(p,({value:n,Icon:_,label:h})=>s("button",{key:n,onClick:F=>r(c)(n),class:f(["flex items-center rounded-md px-3.5 py-1.5 transition-colors",r(a)===n?"bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100":"text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60"])},[(t(),d(x(_),{class:"-ml-1 h-4 w-4"})),s("span",z,b(h),1)],10,S)),64))]))}}),L={class:"space-y-6"},K=m({__name:"Appearance",setup(u){const a=[{title:"Appearance settings",href:"/settings/appearance"}];return(c,p)=>(t(),d(I,{breadcrumbs:a},{default:l(()=>[e(r(v),{title:"Appearance settings"}),e(M,null,{default:l(()=>[s("div",L,[e(w,{title:"Appearance settings",description:"Update your account's appearance settings"}),e(D)])]),_:1})]),_:1}))}});export{K as default};
