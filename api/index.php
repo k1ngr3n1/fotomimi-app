c3686779efd7ff502ad4f63fa401c09d6686db85
Renato <92505604+k1ngr3n1@users.noreply.github.com>
2025-08-10 12:16:08 +0200

    fix 2

--- api/index.php
+++ api/index.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+require __DIR__ . "/../public/index.php";
+
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
